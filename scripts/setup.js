0dd2d83c294b8d87ee754d8ff43ff185608e36e1
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-18 11:41:27 -0400

    added default content types

--- scripts/setup.js
+++ scripts/setup.js
@@ -2,9 +2,12 @@
 var CoreControllerHelper = require('periodicjs.core.controllerhelper'),
 	CoreController,
 	Contenttype,
+	AppDBSetting,
 	logger,
 	mongoose,
 	existingPeriodicContentType,
+	usablePeriodicContentType,
+	itemDefaultContenttype,
 	newPeriodicalContentTypeDocument = {
 		title: 'periodical',
 		name: 'periodical',
@@ -24,6 +27,79 @@ var CoreControllerHelper = require('periodicjs.core.controllerhelper'),
 			name: 'static',
 		}]
 	};
+
+var createDefaultItemContentType = function () {
+	var newDefaultItemContentType = {
+		name: 'item_default_contenttypes'
+	};
+
+	CoreController.createModel({
+		model: AppDBSetting,
+		newdoc: newDefaultItemContentType,
+		callback: function (err, newappsettingdocument) {
+			if (err) {
+				logger.error(err);
+			}
+			else {
+				itemDefaultContenttype = newappsettingdocument;
+				logger.silly('add newly created default content type');
+				updateDefaultItemContentType(itemDefaultContenttype);
+			}
+		}
+	});
+};
+
+var updateDefaultItemContentType = function (appsettingdocumenttouse) {
+	var alreadyHasDefault = false;
+	for (var x in appsettingdocumenttouse.value) {
+		if (usablePeriodicContentType._id.toString() === appsettingdocumenttouse.value[x].toString()) {
+			alreadyHasDefault = true;
+		}
+	}
+	if (alreadyHasDefault) {
+		logger.silly('appsettingdocumenttouse.value', appsettingdocumenttouse.value, 'already has', usablePeriodicContentType._id);
+	}
+	else {
+		var objectToModify = {
+			'value': usablePeriodicContentType._id
+		};
+		CoreController.updateModel({
+			model: AppDBSetting,
+			id: appsettingdocumenttouse._id,
+			appendArray: true,
+			updatedoc: objectToModify,
+			callback: function (err, updatedsetting) {
+				if (err) {
+					logger.error(err);
+				}
+				else {
+					console.log('updated settings', updatedsetting);
+				}
+			}
+		});
+	}
+};
+
+var addDefaultItemContentType = function () {
+	CoreController.loadModel({
+		docid: 'item_default_contenttypes',
+		model: AppDBSetting,
+		callback: function (err, appsettingdocument) {
+			if (err) {
+				logger.error(err);
+			}
+			else if (appsettingdocument) {
+				itemDefaultContenttype = appsettingdocument;
+				logger.silly('add default content type');
+				updateDefaultItemContentType(itemDefaultContenttype);
+			}
+			else {
+				logger.silly('create new default item content type');
+				createDefaultItemContentType();
+			}
+		}
+	});
+};
 
 var checkContentTypePeriodical = function (callback) {
 	CoreController.loadModel({
@@ -56,6 +132,7 @@ var setup = function (resources) {
 	// appSettings = resources.settings,
 	CoreController = new CoreControllerHelper(resources);
 	Contenttype = mongoose.model('Contenttype');
+	AppDBSetting = mongoose.model('Setting');
 
 	mongoose.connection.on('open', function () {
 		checkContentTypePeriodical(function (err, doc) {
@@ -73,6 +150,8 @@ var setup = function (resources) {
 						// console.log(existingPeriodicContentType);
 						logger.info('Updated Periodical Content Type');
 						// console.log(updatedcontenttype);
+						usablePeriodicContentType = updatedcontenttype;
+						addDefaultItemContentType();
 					}
 				});
 			}
@@ -84,6 +163,8 @@ var setup = function (resources) {
 					else {
 						logger.info('createdNewContentType');
 						// console.log(newcontenttype);
+						usablePeriodicContentType = newcontenttype;
+						addDefaultItemContentType();
 					}
 				});
 			}
