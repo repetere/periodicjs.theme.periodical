0107c8def207094aa70a9e4a525918a4c406bd9b
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-09-22 13:10:01 -0400

    home page controller

---
+++ controller/periodical.js
@@ -0,0 +1,89 @@
+'use strict';
+var path = require('path'),
+	Utilities = require('periodicjs.core.utilities'),
+	ControllerHelper = require('periodicjs.core.controllerhelper'),
+	CoreUtilities,
+	CoreController,
+	themeController;
+
+var homepage = function(req,res){
+	themeController.customLayout({
+			req: req,
+			res: res,
+			next: false,
+			viewpath: 'home/index',
+			layoutdata: {
+				categories: {
+					model: 'Category',
+					search: {
+						query: req.params.cat,
+						sort: '-createdat',
+						limit: 10,
+						offset: 0
+					}
+				},
+				items: {
+					model: 'Item',
+					search: {
+						query: req.params.item,
+						sort: '-publishat',
+						limit: 10,
+						offset: 0,
+						population: 'authors primaryauthor contenttypes primaryasset tags categories '
+					}
+				},
+				collections: {
+					model: 'Collection',
+					search: {
+						query: req.params.item,
+						sort: '-publishat',
+						limit: 10,
+						offset: 0,
+						population: 'tags categories authors assets primaryasset contenttypes primaryauthor'
+					}
+				},
+				tags: {
+					model: 'Tag',
+					search: {
+						query: req.params.item,
+						sort: '-createdat',
+						limit: 10,
+						offset: 0
+					}
+				},
+				authors: {
+					model: 'User',
+					search: {
+						query: req.params.item,
+						sort: '-createdat',
+						limit: 10,
+						offset: 0
+					}
+				},
+				contenttypes: {
+					model: 'Contenttype',
+					search: {
+						query: req.params.item,
+						sort: '-createdat',
+						limit: 10,
+						offset: 0
+					}
+				}
+			}
+		});
+};
+
+var controller = function (resources) {
+	// logger = resources.logger;
+	// mongoose = resources.mongoose;
+	// appSettings = resources.settings;
+	CoreController = new ControllerHelper(resources);
+	CoreUtilities = new Utilities(resources);
+	themeController = require(path.join(process.cwd(), 'app/controller/theme'))(resources);
+
+	return {
+		homepage: homepage
+	};
+};
+
+module.exports = controller;
--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@
 var path = require('path');
 
 module.exports = function (periodic) {
-	// var themeController = require(path.join(__dirname, '../../../app/controller/theme'))(periodic),
-	var itemController = require(path.join(__dirname, '../../../app/controller/item'))(periodic),
+	var periodicalController = require('./controller/periodical')(periodic),
+		itemController = require(path.join(process.cwd(), 'app/controller/item'))(periodic),
 		themeRouter = periodic.express.Router();
 
 	require('./scripts/setup')(periodic);
@@ -11,9 +11,7 @@ module.exports = function (periodic) {
 	// create new route to document items to post
 	themeRouter.get('periodical/:id', itemController.loadFullItem, itemController.show);
 	themeRouter.get('/items', itemController.loadItems, itemController.index);
-	// themeRouter.get('/', function (req, res) {
-	// 	res.send('homepage');
-	// });
+	themeRouter.get('/', periodicalController.homepage);
 
 	periodic.app.use(themeRouter);
 };
